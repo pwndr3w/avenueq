50f134d2599f25dbcc6b2e82335574d9ae1f4b1f
김민성 <minsung@gimminseong-ui-MacBookPro.local>
2020-03-17 23:51:30 +0900

    router test 6 - hashrouter

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect, HashRouter } from "react-router-dom";
 
 // styles for this kit
 import "assets/css/bootstrap.min.css";
@@ -32,27 +32,32 @@ import LandingPage from "./views/LandingPage.js";
 import ProfilePage from "./views/examples/ProfilePage.js";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Switch>
-      <Route path="/index" render={props => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route path="/login-page" render={props => <LoginPage {...props} />} />
-        <Redirect to="/landing-page" />
-        <Redirect from="/" to="/landing-page" />
-      </Switch>
-    </Switch>
-  </BrowserRouter>,
+  <HashRouter>
+    <Route path="/" exact="true" component={Index}/>
+    <Route path="/landingPage" exact="true" component={LandingPage}/>
+  </HashRouter>,
+
+  // <BrowserRouter>
+  //   <Switch>
+  //     <Switch>
+  //     <Route path="/index" render={props => <Index {...props} />} />
+  //       <Route
+  //         path="/nucleo-icons"
+  //         render={props => <NucleoIcons {...props} />}
+  //       />
+  //       <Route
+  //         path="/landing-page"
+  //         render={props => <LandingPage {...props} />}
+  //       />
+  //       <Route
+  //         path="/profile-page"
+  //         render={props => <ProfilePage {...props} />}
+  //       />
+  //       <Route path="/login-page" render={props => <LoginPage {...props} />} />
+  //       <Redirect to="/landing-page" />
+  //       <Redirect from="/" to="/landing-page" />
+  //     </Switch>
+  //   </Switch>
+  // </BrowserRouter>,
   document.getElementById("root")
 );
