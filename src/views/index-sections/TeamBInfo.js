18779fbab165dcdff4d8a6d8ff638b2836bc22a7
ParkKukHee <62241959+ParkKukHee@users.noreply.github.com>
2020-03-20 22:36:56 +0900

    issue 8
    re T^T

--- src/views/index-sections/TeamBInfo.js
+++ src/views/index-sections/TeamBInfo.js
@@ -357,13 +357,13 @@ function TeamB() {
                                     <img
                                         alt="..."
                                         className="rounded img-raised"
-                                        src={require("assets/img/윤한솔.jpg")}
+                                        src={require("assets/img/김재혁.jpg")}
                                     ></img>
                                     <br></br>
                                     <br></br>
                                     <Col className="text-center" lg="12" md="12">
                                         <h3 className="title">앙상블</h3>
-                                        <p className="category text-info">윤한솔</p>
+                                        <p className="category text-info">김재혁</p>
                                     </Col>
                                 </Col>
 
@@ -371,7 +371,7 @@ function TeamB() {
                                     <img
                                         alt="..."
                                         className="rounded img-raised"
-                                        src={require("assets/img/김재혁.jpg")}
+                                        src={require("assets/img/박은영.jpg")}
                                     ></img>
                                     <br></br>
                                     <br></br>
