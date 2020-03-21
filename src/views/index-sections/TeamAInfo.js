f0c0effae00dc9f955aa92f6cd2d8955d9190371
김민성 <minsung@gimminseong-ui-MacBookPro.local>
2020-03-21 12:03:45 +0900

    scroll to top added

--- src/views/index-sections/TeamAInfo.js
+++ src/views/index-sections/TeamAInfo.js
@@ -25,6 +25,7 @@ function TeamAInfo() {
         document.body.classList.add("landing-page");
         document.body.classList.add("sidebar-collapse");
         document.documentElement.classList.remove("nav-open");
+        window.scrollTo(0, 0)
         return function cleanup() {
             document.body.classList.remove("landing-page");
             document.body.classList.remove("sidebar-collapse");
